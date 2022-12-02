973ec125c2b93b1c456d13d57e89001d6df24921
Phillo Tunru <tunru@usc.edu>
2022-12-02 04:37:22 +0000

    wordle function implemented

--- wordle.cpp
+++ wordle.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 // For std::remove
 #include <algorithm> 
-#include <map>
+#include <unordered_map>
 #include <set>
 #include "wordle.h"
 #include "dict-eng.h"
@@ -10,8 +10,32 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+void solveWordle(string s, const string& in, string floating, const set<string>& dict, set<string>& ret){
+    int n = s.size();
+    if (n==in.size()){
+        
+        if (!floating.empty() || dict.find(s) == dict.end()) return;
+        ret.insert(s);
+        return;
+    }
+    if (in[n]!='-'){
+        solveWordle(s+in[n], in, floating, dict, ret);
+        return;
+    }
+    for (char c = 'a'; c<= 'z'; c++ ){
+        auto x = floating.find(c);
+        if (x!=string::npos){
+            string temp = floating;
+            floating.erase(x,1);
+            solveWordle(s+c, in, floating, dict, ret);
+            floating = temp;
+            continue;
+        }
+        solveWordle(s+c, in, floating, dict, ret);
+    }
+    return;
 
-
+}
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
@@ -19,6 +43,11 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    
+    set<string> ret;
+
+    solveWordle("", in, floating, dict, ret);
+    return ret;
 
 }
 
